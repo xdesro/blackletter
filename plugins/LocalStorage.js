5ae920133a82359bec906813616d4fc466a8e792
Henry Desroches <yo@henry.codes>
2019-05-26 16:46:45 -0600

    🖼  - SVG hero header and fix persisted state bugs

--- plugins/LocalStorage.js
+++ plugins/LocalStorage.js
@@ -1,7 +1,12 @@
 import createPersistedState from "vuex-persistedstate";
-
 export default ({ store }) => {
   window.onNuxtReady(() => {
+    const state = localStorage.getItem("vuex");
+    const JSONstate = JSON.parse(state);
+    delete JSONstate.pageNumber;
+    delete JSONstate.pageTitle;
+    delete JSONstate.backgroundText;
+    localStorage.setItem("vuex", JSON.stringify(JSONstate));
     createPersistedState()(store);
   });
 };
