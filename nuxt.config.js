de22198ea548e4475bd5bb5e30d43be2ed52757b
Henry Desroches <yo@henry.codes>
2019-05-26 13:12:00 -0600

    🤗  - Add Facebook meta and update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,19 +13,19 @@ export default {
       { hid: "description", name: "description", content: pkg.description },
       { name: "theme-color", content: "#030303" },
       { name: "msapplication-TileColor", content: "#030303" },
-      { name: "image", content: "https://henry.codes/opengraph.jpg" },
-      { itemprop: "name", content: "Creative Developer // Henry Desroches" },
+      { name: "image", content: "https://henry.codes/opengraph-1.png" },
+      { itemprop: "name", content: "Henry Desroches | Creative Developer" },
       {
         itemprop: "description",
         content:
           "Personal site and portfolio of UX engineer and prolific twitter user xdesro."
       },
-      { itemprop: "image", content: "https://henry.codes/opengraph.jpg" },
+      { itemprop: "image", content: "https://henry.codes/opengraph-1.png" },
 
       { name: "twitter:card", content: "summary" },
       {
         name: "twitter:title",
-        content: "Creative Developer // Henry Desroches"
+        content: "Henry Desroches | Creative Developer"
       },
       {
         name: "twitter:description",
@@ -35,17 +35,17 @@ export default {
       { name: "twitter:site", content: "@xdesro" },
       { name: "twitter:creator", content: "@xdesro" },
       { name: "twitter:image:src", content: "https://henry.codes/twitter.jpg" },
-      { name: "og:title", content: "Creative Developer // Henry Desroches" },
+      { name: "og:title", content: "Henry Desroches | Creative Developer" },
       {
         name: "og:description",
         content:
           "Personal site and portfolio of UX engineer and prolific twitter user xdesro."
       },
-      { name: "og:image", content: "https://henry.codes/opengraph.jpg" },
+      { name: "og:image", content: "https://henry.codes/opengraph-1.png" },
       { name: "og:url", content: "https://henry.codes" },
       {
         name: "og:site_name",
-        content: "Creative Developer // Henry Desroches"
+        content: "Henry Desroches | Creative Developer"
       },
       { name: "og:locale", content: "en_US" },
       { name: "og:type", content: "website" }
